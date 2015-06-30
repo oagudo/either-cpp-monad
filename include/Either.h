ab1ab12ecb179bf7d0080a37012e79fc18904c12
oagudo <omar.agudo@gmail.com>
2015-06-30 18:42:14 +0200

    Minor either class modifications

---
+++ include/Either.h
@@ -0,0 +1,20 @@
+#ifndef _EITHER_H
+#define _EITHER_H
+
+#include <boost/optional.hpp>
+
+template <typename TLeft, typename TRight>
+class Either {
+    public:
+        Either(const TLeft&& l) : _l(l) {}
+        Either(const TRight&& r) : _r(r) { }
+        bool IsLeft() const { return _l; }
+        bool IsRight() const { return _r; }
+        TLeft Left() const { return _l.get();}
+        TRight Right() const { return _r.get();}
+   private:
+        boost::optional<TLeft> _l;
+        boost::optional<TRight> _r;
+};
+
+#endif
